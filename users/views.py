6e3189e5cb1c0df9c6c63b793750f0c4acdfe5d8
lenwe171175 <robin.sylvestre@live.fr>
2022-07-30 17:05:58 +0200

    Add 500 error page

--- users/views.py
+++ users/views.py
@@ -10,7 +10,7 @@ from .models import Utilisateur
 def page_not_found_view(request, exception):
     return render(request, "404.html", status=404)
 
-def server_error_view(request, exception):
+def server_error_view(request):
     return render(request, "500.html", status=500)
 
 @login_required
